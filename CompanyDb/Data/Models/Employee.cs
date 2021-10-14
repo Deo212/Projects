cd116ce2db4c78585df33755e70f7d43f625131d
Deo212 <d.stoyanov2017@pmg-pernik.com>
2021-10-14 14:05:11 +0300

    add zadacha 2

---
+++ CompanyDb/Data/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyDb.Data.Models
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Town { get; set; }
+        public decimal Salary { get; set; }
+        public  int DepartmentName { get; set; }
+    }
+}
